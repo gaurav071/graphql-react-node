bca00b6477f8f95820a59c816961d96972528065
Gaurav Pandey <gaurav071@gmail.com>
2021-04-01 13:05:32 +0530

    initial client app setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-function App() {
+import Home from "./pages/Home";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+});
+
+const App = () => {
   return (
-    <p>Hello React</p>
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
